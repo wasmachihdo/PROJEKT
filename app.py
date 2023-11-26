a2ed24fd5a8cbb8470660238f506c8a03532fec4
Zenon <zenon.truffer@stud.fhgr.ch>
2023-11-26 09:30:19 +0100

    query models and datetime

--- app.py
+++ app.py
@@ -1,43 +1,54 @@
-from flask import Flask, render_template #Import flask
+from flask import Flask, render_template, request #Import flask
 from flask_sqlalchemy import SQLAlchemy #Import Datenbank
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import Integer, String, DateTime #Datenarten: Zahl, Zeichenkette, Datum
 from sqlalchemy.orm import Mapped, mapped_column
-import datetime
+import datetime #Modul für Datum (Ablaufdatum des Tasks)
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite" #Generierung der Datei
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite" #Generierung der Datei zur Datenspeicherung
 db = SQLAlchemy() #Datenbank kreieren
 db.init_app(app)
 
-class Todo(db.Model):
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Todo(db.Model): #Tabellendaten: ID, Titel, Ablaufdatum
     __tablename__="todo"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
-    title: Mapped[str] = mapped_column(String(100), nullable=False)
-    due: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False),nullable=True) 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) #id wird automatisch vergeben
+    title: Mapped[str] = mapped_column(String(80), nullable=False) #maximal 80 Zeichen für die Eingabe des Tasks
+    due: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False),nullable=True) #Eingabe Datum 
 
-
-
-
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#QUERY MODELS -> 24.11
 
 @app.route('/')
-def index(): #show all todos 
-    db.query(Todo) # recherchieren Query Models 
-    return render_template("base.html") # HTML in Template-Dateien ausgelagert und mit render_template versendet
+def index(): #alle Tasks  
+    todo_list = Todo.query.all() #Liste generieren  
+    return render_template("base.html", todo_list=todo_list) # HTML in Template-Dateien ausgelagert und mit render_template versendet
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#Task hinzufügen
+#def add()
+#complete=false
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#datetime
 
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        todo = Todo(title="task 1")
-        db.session.add(todo)
+        new_todo = Todo(title="task 1") #Hinzugügen eines neuen Tasks (Nummer weitergehen??)
+        db.session.add(new_todo)
         db.session.commit()
     app.run(debug=True, port=5000)
  
- 
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 
