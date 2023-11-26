f3feba1bca4a27ffb101b219c58f43620c4b6257
Zenon <zenon.truffer@stud.fhgr.ch>
2023-11-26 22:19:29 +0100

    Done und Delete Button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request #Import flask
+from flask import Flask, render_template, request, redirect, url_for  #Import flask
 from flask_sqlalchemy import SQLAlchemy #Import Datenbank
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String, DateTime #Datenarten: Zahl, Zeichenkette, Datum
@@ -19,6 +19,7 @@ class Todo(db.Model): #Tabellendaten: ID, Titel, Ablaufdatum
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) #id wird automatisch vergeben
     title: Mapped[str] = mapped_column(String(80), nullable=False) #maximal 80 Zeichen für die Eingabe des Tasks
     due: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False),nullable=True) #Eingabe Datum 
+    complete: Mapped[bool] = mapped_column(db.Boolean, default=False)
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 #QUERY MODELS -> 24.11
@@ -30,8 +31,35 @@ def index(): #alle Tasks
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 #Task hinzufügen
-#def add()
-#complete=false
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+#Task updaten
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+#Task löschen
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 #datetime
@@ -42,10 +70,7 @@ def index(): #alle Tasks
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        new_todo = Todo(title="task 1") #Hinzugügen eines neuen Tasks (Nummer weitergehen??)
-        db.session.add(new_todo)
-        db.session.commit()
-    app.run(debug=True, port=5000)
+        app.run(debug=True, port=5000)
  
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 
